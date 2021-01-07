9a4b1953d78943df4bf05611ba515f339f6f4bb0
norenstrakh-r7 <nikolai_orenstrakh@rapid7.com>
2021-01-07 16:17:17 -0500

    add C module

---
+++ c/c.go
@@ -0,0 +1,9 @@
+package c
+
+import (
+	"github.com/norenstrakh-r7/submodules/b"
+)
+
+//Name is name
+const Name = b.Name
+
---
+++ temp/c.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/norenstrakh-r7/submodules/b"
+)
+//Name const
+const Name = "tmp/c name :" + b.Name
+
+func main()  {
+	fmt.Println(Name)
+
+}
